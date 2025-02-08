a779e282af9b0c0c80e6e9d37c6945f9ad68ed6c
alex <alex.hmn18@gmail.com>
2025-02-08 19:42:37 +0100

    Création controller match

---
+++ src/Controller/MatchController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MatchController extends AbstractController
+{
+    #[Route('/match', name: 'match')]
+    public function index(): Response
+    {
+        return $this->render('match/index.html.twig', [
+            'controller_name' => 'MatchController',
+        ]);
+    }
+}
