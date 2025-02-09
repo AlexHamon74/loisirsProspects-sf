382b00adcde3c85af265f5726767510f21477a95
alex <alex.hmn18@gmail.com>
2025-02-09 13:24:36 +0100

    Modification vue mon_equipe : Affiche les joueurs de la même équipe que le user connecté

--- src/Controller/JoueurController.php
+++ src/Controller/JoueurController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +11,19 @@ use Symfony\Component\Routing\Attribute\Route;
 class JoueurController extends AbstractController
 {
     #[Route('/mon_equipe', name: 'mon_equipe')]
-    public function index(): Response
+    public function mon_equipe(UserRepository $userRepository): Response
     {
+        $user = $this->getUser();
+        
+        if (!$user instanceof User) {
+            throw $this->createNotFoundException("Utilisateur non trouvé.");
+        }
+
+        $equipe = $user->getEquipe();
+        $users = $userRepository->findBy(['equipe' => $equipe]);
+
         return $this->render('joueur/mon_equipe.html.twig', [
+            'users' => $users,
             'controller_name' => 'Mon Equipe',
         ]);
     }
