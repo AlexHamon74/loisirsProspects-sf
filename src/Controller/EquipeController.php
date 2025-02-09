018743dec2c1e0d9942efd627e4c0814d77ce59d
alex <alex.hmn18@gmail.com>
2025-02-09 13:34:15 +0100

    Affichage des joueurs par équipes

--- src/Controller/EquipeController.php
+++ src/Controller/EquipeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Equipe;
 use App\Repository\EquipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,12 +13,23 @@ class EquipeController extends AbstractController
     #[Route('/equipes', name: 'equipes')]
     public function equipes(EquipeRepository $repository): Response
     {
-
         $equipes = $repository->findAll();
 
         return $this->render('equipe/equipes.html.twig', [
             'controller_name' => 'LoisirsProspects - Equipes',
             'equipes' => $equipes,
+        ]);
+    }
+
+    #[Route('/equipe/{id}', name: 'equipe')]
+    public function equipe(Equipe $equipe): Response
+    {
+        $users = $equipe->getUsers();
+
+        return $this->render('equipe/equipe.html.twig', [
+            'controller_name' => 'LoisirsProspects - Equipes',
+            'equipe' => $equipe,
+            'users' => $users,
         ]);
     }
 }
