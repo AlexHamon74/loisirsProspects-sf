275e397b3279794cc46d4b6105d9afbe2786408f
alex <alex.hmn18@gmail.com>
2025-02-15 19:02:20 +0100

    Datatable en fr

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,14 @@ import './styles/app.css';
 $(document).ready(function() {
     $('#usersTable').DataTable({
         responsive: true,
-        info: false,
+        language: {
+            info: 'Affichage de la page _PAGE_ sur _PAGES_',
+            infoEmpty: 'Aucun enregistrement disponible',
+            infoFiltered: '(filtré à partir de _MAX_ enregistrements au total)',
+            lengthMenu: 'Afficher _MENU_ lignes par page',
+            zeroRecords: 'Aucun résultat trouvé',
+            search: 'Rechercher:'
+        }
     });
 
 });
